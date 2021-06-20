56e03c13088baea72e3f53c94d461f7a9f396f57
Addan Felipe Neri Andrade <addanfelipe@gmail.com>
2021-06-20 18:18:49 -0300

    feat(hotel/cheapest): count week weekend

--- bookhotel_api/hotel/views.py
+++ bookhotel_api/hotel/views.py
@@ -1,7 +1,8 @@
-from rest_framework.permissions import AllowAny
+from datetime import date, datetime
+
 from rest_framework import views
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework import status
 
 # Create your views here.
 
@@ -10,7 +11,38 @@ class Cheapest(views.APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        params = request.query_params['input']
+        input = request.query_params['input']
+        [client_type, dates_str] = input.split(': ')
+        dates_str = dates_str.split(', ')
+
+        def str_to_date(date_str):
+            date_str = date_str[:9]
+            return datetime.strptime(date_str, '%d%b%Y').date()
+
+        def count_week_type(dates: date):
+            count_week, count_weekend = 0, 0
+            for date in dates:
+                if date.weekday() in [5, 6]:
+                    count_weekend += 1
+                else:
+                    count_week += 1
+
+            return [count_week, count_weekend]
+
+        dates = [str_to_date(_date_str) for _date_str in dates_str]
+        [count_week, count_weekend] = count_week_type(dates)
+
+        print(count_week, count_weekend)
+
+        # datetime_object = datetime.strptime('17Mar2009', '%d%b%Y').date()
+
+        # print(datetime_object)
+
+        # print(date(2009, 3, 16).weekday())
+
+        # print(client_type)
+        # print(dates)
+
         return Response({
             "cheapest": "Lakewood"
         })
