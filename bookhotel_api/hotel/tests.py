8023899bf9c5542ed93a3ea789f8d119b3641c74
Addan Felipe Neri Andrade <addanfelipe@gmail.com>
2021-06-21 02:11:38 -0300

    feat(hotel/cheapest): test unit

--- bookhotel_api/hotel/tests.py
+++ bookhotel_api/hotel/tests.py
@@ -7,7 +7,7 @@ class CheapestTest(TestCase):
     def setUp(self):
         pass
 
-    def get(self, input, result):
+    def get_200(self, input, result):
         c = Client()
         response = c.get('/api/v1/cheapest/', {
             'input': input
@@ -19,7 +19,23 @@ class CheapestTest(TestCase):
         self.assertEqual(data['cheapest'], result)
 
 
-    def test_get(self):
-        self.get('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)', 'Lakewood')
-        self.get('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)', 'Bridgewood')
-        self.get('Reward: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)', 'Ridgewood')
+    def test_get_lakewood(self):
+        self.get_200('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)', 'Lakewood')
+
+    def test_get_bridgewood(self):
+        self.get_200('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)', 'Bridgewood')
+
+    def test_get_ridgewood(self):
+        self.get_200('Reward: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)', 'Ridgewood')
+
+    def test_get_400(self):
+        c = Client()
+
+        response = c.get('/api/v1/cheapest/', {
+        })
+        self.assertEqual(response.status_code, 400)
+        
+        response = c.get('/api/v1/cheapest/', {
+            'input': 'Reguar: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)'
+        })
+        self.assertEqual(response.status_code, 400)
