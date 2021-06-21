5f75020a3827f8dce6bc504b97010a76f2cb95a2
Addan Felipe Neri Andrade <addanfelipe@gmail.com>
2021-06-20 23:24:17 -0300

    refactor(hotel/cheapest): filter

--- bookhotel_api/hotel/models.py
+++ bookhotel_api/hotel/models.py
@@ -1,6 +1,34 @@
+from typing import Literal
 from django.db import models
 
 # Create your models here.
+
+
+class HotelManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_cheapest(self, client_type: Literal['Regular', 'Reward'], count_week: int, count_weekend: int):
+        if client_type == 'Regular':
+            column_week, column_weekend = 'rate_week_regular', 'rate_weekend_regular'
+        else:
+            column_week, column_weekend = 'rate_week_loyalty', 'rate_weekend_loyalty'
+
+        sql = f'''
+SELECT
+        hotel_hotel.id,
+        hotel_hotel.name
+FROM
+        hotel_hotel
+GROUP BY
+        hotel_hotel.id
+ORDER BY
+        SUM(hotel_hotel.{column_week}) * {count_week} + SUM(hotel_hotel.{column_weekend}) * {count_weekend},
+        hotel_hotel.excellence_rating DESC
+LIMIT 1
+'''
+        result = self.raw(sql)
+        return result[0]
 
 
 class Hotel(models.Model):
@@ -10,3 +38,5 @@ class Hotel(models.Model):
     rate_week_loyalty = models.FloatField()
     rate_weekend_regular = models.FloatField()
     rate_weekend_loyalty = models.FloatField()
+
+    objects = HotelManager()
--- bookhotel_api/hotel/views.py
+++ bookhotel_api/hotel/views.py
@@ -1,6 +1,4 @@
 from datetime import date, datetime
-from enum import Enum
-from typing import NamedTuple, NewType
 
 from rest_framework import views
 from rest_framework.permissions import AllowAny
@@ -44,26 +42,8 @@ class Cheapest(views.APIView):
         input = request.query_params['input']
         client_type, count_week, count_weekend = extract_query(input)
 
-        if client_type == 'Regular':
-            column_week, column_weekend = 'rate_week_regular', 'rate_weekend_regular'
-        else:
-            column_week, column_weekend = 'rate_week_loyalty', 'rate_weekend_loyalty'
-
-        sql = f'''
-SELECT
-        hotel_hotel.id,
-        hotel_hotel.name
-FROM
-        hotel_hotel
-GROUP BY
-        hotel_hotel.id
-ORDER BY
-        SUM(hotel_hotel.{column_week}) * {count_week} + SUM(hotel_hotel.{column_weekend}) * {count_weekend},
-        hotel_hotel.excellence_rating DESC
-LIMIT 1
-'''
-        result = Hotel.objects.raw(sql)
+        hotel = Hotel.objects.get_cheapest(client_type, count_week, count_weekend)
 
         return Response({
-            "cheapest": result[0].name
+            "cheapest": hotel.name
         })
