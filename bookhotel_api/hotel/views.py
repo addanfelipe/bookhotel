c1f6d7fbd722db438902042ceb662e3920f78c04
Addan Felipe Neri Andrade <addanfelipe@gmail.com>
2021-06-20 23:10:43 -0300

    refactor(hotel/cheapest): extract query

--- bookhotel_api/hotel/views.py
+++ bookhotel_api/hotel/views.py
@@ -1,11 +1,40 @@
-from hotel.models import Hotel
 from datetime import date, datetime
+from enum import Enum
+from typing import NamedTuple, NewType
 
 from rest_framework import views
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
+from hotel.models import Hotel
+
 # Create your views here.
+
+
+def str_to_date(date_str):
+    date_str = date_str[:9]
+    return datetime.strptime(date_str, '%d%b%Y').date()
+
+
+def count_week_type(dates: date):
+    count_week, count_weekend = 0, 0
+    for date in dates:
+        if date.weekday() < 5:
+            count_week += 1
+        else:
+            count_weekend += 1
+
+    return count_week, count_weekend
+
+
+def extract_query(input: str):
+    [client_type, dates_str] = input.split(': ')
+    dates_str = dates_str.split(', ')
+
+    dates = [str_to_date(_date_str) for _date_str in dates_str]
+    count_week, count_weekend = count_week_type(dates)
+
+    return client_type, count_week, count_weekend
 
 
 class Cheapest(views.APIView):
@@ -13,30 +42,12 @@ class Cheapest(views.APIView):
 
     def get(self, request):
         input = request.query_params['input']
-        [client_type, dates_str] = input.split(': ')
-        dates_str = dates_str.split(', ')
+        client_type, count_week, count_weekend = extract_query(input)
 
-        def str_to_date(date_str):
-            date_str = date_str[:9]
-            return datetime.strptime(date_str, '%d%b%Y').date()
-
-        def count_week_type(dates: date):
-            count_week, count_weekend = 0, 0
-            for date in dates:
-                if date.weekday() in [5, 6]:
-                    count_weekend += 1
-                else:
-                    count_week += 1
-
-            return [count_week, count_weekend]
-
-        dates = [str_to_date(_date_str) for _date_str in dates_str]
-        [count_week, count_weekend] = count_week_type(dates)
-        
         if client_type == 'Regular':
-            column_week, column_weekend = ('rate_week_regular', 'rate_weekend_regular')
+            column_week, column_weekend = 'rate_week_regular', 'rate_weekend_regular'
         else:
-            column_week, column_weekend = ('rate_week_loyalty', 'rate_weekend_loyalty')
+            column_week, column_weekend = 'rate_week_loyalty', 'rate_weekend_loyalty'
 
         sql = f'''
 SELECT
